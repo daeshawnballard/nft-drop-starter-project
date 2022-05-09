4675a345a154daf7ed40b466b6ae6a480a5137bd
imdaeshawn <imdaeshawn@gmail.com>
2022-05-09 11:10:27 -0400

    changed the drop date to test minting once more, have to do some css work and fix the undefined on the items minted, fixed one error for it already.

--- app/src/CandyMachine/index.js
+++ app/src/CandyMachine/index.js
@@ -3,6 +3,7 @@ import { Connection, PublicKey } from '@solana/web3.js';
 import { Program, Provider, web3 } from '@project-serum/anchor';
 import { MintLayout, TOKEN_PROGRAM_ID, Token } from '@solana/spl-token';
 import { sendTransactions } from './connection';
+import CountdownTimer from '../CountdownTimer';
 import './CandyMachine.css';
 import {
   candyMachineProgram,
@@ -28,6 +29,12 @@ const CandyMachine = ({ walletAddress }) => {
   }, []);
 
   const [candyMachine, setCandyMachine] = useState(null);
+  // State
+  const [machineStats, setMachineStats] = useState(null);
+  const [mints, setMints] = useState([]);
+  const [isMinting, setIsMinting] = useState(false);
+  const [isLoadingMints, setIsLoadingMints] = useState(false);
+
   const getProvider = () => { 
     const rpcHost = process.env.REACT_APP_SOLANA_RPC_HOST;
     //Create a new connection object
@@ -389,16 +396,49 @@ const CandyMachine = ({ walletAddress }) => {
     }
     return [];
   };
+  
+  const renderMintedItems = () => (
+    <div className="gif-container">
+        <p className="sub-text">Minted Items ✨</p>
+        <div className="gif-grid">
+            {mints.map((mint) => (
+                <div className="gif-item" key={mint}>
+                    <img src={mint} alt={`Minted NFT ${mint}`} />
+                </div>
+            ))}
+        </div>
+    </div>
+);
+
+  // Create render function
+const renderDropTimer = () => {
+  // Get the current date and dropDate in a JavaScript Date object
+  const currentDate = new Date();
+  const dropDate = new Date(candyMachine.state.goLiveData * 1000);
+
+  // If currentDate is before dropDate, render our Countdown component
+  if (currentDate < dropDate) {
+    console.log('Before drop date!');
+    // Don't forget to pass over your dropDate!
+    return <CountdownTimer dropDate={dropDate} />;
+  }
+
+  // Else let's just return the current drop date
+  return <p>{`Drop Date: ${candyMachine.state.goLiveDateTimeString}`}</p>;
+};
 
   return (
     // Only show if machine states is available
     candyMachine && (
     <div className="machine-container">
+      {renderDropTimer()}
       <p>{`Drop Date: ${candyMachine.state.goLiveDateTimeString}`}</p>
-      <p>{`Items Minted: ${candyMachine.state.itemsRedeemed} / ${candyMachine.state.itemsAvalible}`}</p>
+      <p>{`Items Minted: ${candyMachine.state.itemsRedeemed} / ${candyMachine.state.itemsAvaliable}`}</p>
       <button className="cta-button mint-button" onClick={mintToken}>
         Mint NFT
       </button>
+      {mints.length > 0 && renderMintedItems()}
+      {isLoadingMints && <p>LOADING MINTS...</p>}
     </div>
     )
   );
